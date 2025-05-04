c7fa2547de87e618c6c829aeeef510192e67a208
MaiThanhSinh <cmthhd2024@gmail.com>
2025-05-04 16:05:48 +0700

    Add files via upload
    Signed-off-by: MaiThanhSinh <cmthhd2024@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
-  let slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName("mySlides");
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
