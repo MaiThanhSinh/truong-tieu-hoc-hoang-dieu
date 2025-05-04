d853276b9aa6df1feed3e40ed92b2535eb10bab0
MaiThanhSinh <cmthhd2024@gmail.com>
2025-05-04 19:23:47 +0700

    Add files via upload
    Signed-off-by: MaiThanhSinh <cmthhd2024@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
 let slideIndex = 0;
 showSlides();
+
 function showSlides() {
+  let i;
   let slides = document.getElementsByClassName("mySlides");
-  for (let i = 0; i < slides.length; i++) {
+  
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
+  
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 3000); // Đổi ảnh sau 3 giây
 }
